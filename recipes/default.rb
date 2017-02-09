ed9b1243dcd6f007d8f4156360354d147098abcf
mhamdan123 <root@ip-172-31-9-144.ec2.internal>
2017-02-09 14:22:07 +0000

    cookbook for git and nginx vhost

---
+++ recipes/default.rb
@@ -0,0 +1,48 @@
+#
+# Cookbook Name:: deploy_CB
+# Recipe:: default
+file '/root/.ssh/config' do
+        action :delete
+end
+
+file '/root/.ssh/git_var' do
+        action :delete
+end
+
+cookbook_file '/root/.ssh/config' do
+        source 'config'
+        action :create
+end
+
+cookbook_file '/root/.ssh/git_var' do
+        source 'git_var'
+	mode '0600'
+	owner 'root'
+	group 'root'
+	action :create
+end
+
+git "/var/www/html/#{node["project_name"]}" do
+  repository node["git_uri"]
+  revision 'master'
+  #action :checkout
+end
+
+file '/root/.ssh/git_var' do
+        action :delete
+end
+
+file '/root/.ssh/config' do
+        action :delete
+end
+
+template "/etc/nginx/conf.d/#{node["server_name"]}.conf" do
+  source 'vhost.erb'
+  owner 'nginx'
+  group 'nginx'
+  mode '0775'
+end
+
+service "nginx" do
+        action :reload
+end
